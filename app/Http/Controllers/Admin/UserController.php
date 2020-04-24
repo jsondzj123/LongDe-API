23b4c20d20671ba807386e2008c09db125d41c7b
jsondzj123 <duzhijian11@163.com>
2020-04-24 10:03:22 +0800

    增加后端学员文件

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+#use App\Models\User;
+#use App\Models\Auth;
+
+#use DB;
+#use Illuminate\Support\Facades\Redis;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+
+use Tymon\JWTAuth\JWTAuth;
+
+
+
+class UserController extends Controller {
+    protected $jwt;
+    
+    /*public function __construct(JWTAuth $jwt) {
+        $this->jwt = $jwt;
+    }*/
+    
+    public function loginAndRegister(){
+        //echo 'nnn';
+       //Redis::set('name', 'guwenjie');
+        $values = Redis::get('name');
+        dd($values);
+
+    }
+    
+    /*
+     * @param 根据用户ID获取用户信息
+     * @param $user_id  
+     */
+    public function getUserList(Request $request){
+        echo "aaaaa";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,10 @@ $router->group(['prefix' => 'web'], function () use ($router) {
 });
 
 //后台端路由接口
-$router->group(['prefix' => 'admin'], function () use ($router) {
-
+$router->group(['prefix' => 'admin' , 'namespace' => 'Admin'], function () use ($router) {
+    //用户学员相关模块
+    $router->group(['prefix' => 'user'], function () use ($router) {
+        //获取学员列表
+        $router->get('getUserList', 'UserController@getUserList');
+    });
 });
