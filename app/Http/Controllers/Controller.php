79bc122257fd18cf1c642b1e5a4b0f3dff1bc08d
sxl <1275920140@qq.com>
2020-04-30 10:07:57 +0800

    修改继承controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use http\Client\Response;
 use Laravel\Lumen\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\Config;
 use Illuminate\Http\Request;
 
 class Controller extends BaseController {
@@ -19,8 +18,7 @@ class Controller extends BaseController {
      * return  string
      */
     public function __construct(Request $request) {
-
-       $this->accept_data = app('rsa')->servicersadecrypt($request);
+       //$this->accept_data = app('rsa')->servicersadecrypt($request);
        //  app('rsa')->Test();
     }
 
