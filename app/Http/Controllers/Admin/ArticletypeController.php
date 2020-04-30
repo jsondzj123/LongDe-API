2bd043830d87a774a326b59a15a02fa00883369e
Suzhenwen <2663930382@qq.com>
2020-04-30 14:05:14 +0800

    test

--- app/Http/Controllers/Admin/ArticletypeController.php
+++ app/Http/Controllers/Admin/ArticletypeController.php
@@ -2,9 +2,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticletypeController extends Controller {
+    /*
+         * @param  获取列表
+         * @param  $user_id     参数
+         * @param  author  苏振文
+         * @param  ctime   2020/4/29 14:29
+         * return  array
+         */
+    public function getTypeList(){
 
+    }
 }
