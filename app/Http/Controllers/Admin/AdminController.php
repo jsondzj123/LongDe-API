92d6d3c887953e26446b7c8b811416c6af991d2b
sxl <1275920140@qq.com>
2020-05-04 14:12:56 +0800

    admin user controller

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,34 +3,19 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Admin;
-#use App\Models\Auth;
-
-#use DB;
-#use Illuminate\Support\Facades\Redis;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-
-
-use Tymon\JWTAuth\JWTAuth;
 
 
 
 class AdminController extends Controller {
             
 
-
-    public function show($id)
-    {
-        $admin = Admin::findOrFail($id);
-        return $this->response($admin);
-    }
-
     /*
      * @param 根据用户ID获取用户信息
      * @param $user_id
      */
-    public function getUserList(Request $request){
-        echo "aaaaa";
+    public function show($id){
+        $admin = Admin::findOrFail($id);
+        return $this->response($admin);
     }
 }
