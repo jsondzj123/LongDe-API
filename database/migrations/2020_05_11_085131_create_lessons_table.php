776d37bfc6b4f8c00319c62ceb188f7d662239ff
sxl <1275920140@qq.com>
2020-05-11 19:13:15 +0800

    小节/章路由修改&表结构修改

--- database/migrations/2020_05_11_085131_create_lessons_table.php
+++ database/migrations/2020_05_11_085131_create_lessons_table.php
@@ -21,6 +21,7 @@ class CreateLessonsTable extends Migration
             $table->string('cover')->nullable()->comment('封面');
             $table->text('description')->nullable()->comment('描述');
             $table->text('introduction')->nullable()->comment('介绍');
+            $table->text('url')->nullable()->comment('课程资料');
             $table->smallInteger('is_public')->nullable()->default(0)->comment('公开0否1是');
             $table->decimal('price', 10, 2)->nullable()->comment('定价');
             $table->decimal('favorable_price', 10, 2)->nullable()->comment('优惠价格');
