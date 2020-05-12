a2674ae6fc7432a3d26d303e5aaf0e52cbba361c
sxl <1275920140@qq.com>
2020-05-12 15:49:12 +0800

    课程表名修改

--- database/migrations/2020_05_11_085131_create_ld_lessons_table.php
+++ database/migrations/2020_05_11_085131_create_ld_lessons_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLessonsTable extends Migration
+class CreateLdLessonsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateLessonsTable extends Migration
      */
     public function up()
     {
-        Schema::create('lessons', function (Blueprint $table) {
+        Schema::create('ld_lessons', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('admin_id')->comment('操作员ID');
             $table->string('title')->nullable()->comment('课程名称');
