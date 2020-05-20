0857346d33ae9bf546a3406ab367a7094cc81b36
jsondzj123 <duzhijian11@163.com>
2020-05-20 17:18:23 +0800

    增加题库和科目是否存在判断

--- app/Models/Chapters.php
+++ app/Models/Chapters.php
@@ -4,6 +4,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\AdminLog;
 use App\Models\Exam;
+use App\Models\Bank;
+use App\Models\QuestionSubject;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\DB;
 
@@ -226,6 +228,18 @@ class Chapters extends Model {
         
         //开启事务
         DB::beginTransaction();
+        
+        //判断题库id对应的题库是否存在
+        $bank_count = Bank::where("id",$body['bank_id'])->count();
+        if($bank_count <= 0){
+            return ['code' => 204 , 'msg' => '此题库信息不存在'];
+        }
+        
+        //判断科目id对应的科目是否存在
+        $bank_count = QuestionSubject::where("id",$body['subject_id'])->count();
+        if($bank_count <= 0){
+            return ['code' => 204 , 'msg' => '此科目信息不存在'];
+        }
 
         //将数据插入到表中
         $chapters_id = self::insertChapters($chapters_array);
