c31e734b78647bde9e67021f8b2708d69de4d00e
sxl <1275920140@qq.com>
2020-05-22 11:18:12 +0800

    优化未授权返回提示

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,6 +8,7 @@ use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -48,7 +49,20 @@ class Handler extends ExceptionHandler
      * @throws \Throwable
      */
     public function render($request, Throwable $exception)
-    {
-        return parent::render($request, $exception);
+    {   
+        if ($exception instanceof UnauthorizedHttpException) {
+            $preException = $exception->getPrevious();
+            if ($preException instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+                return response()->json(['error' => 'TOKEN_EXPIRED']);
+            } else if ($preException instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+                return response()->json(['error' => 'TOKEN_INVALID']);
+            } else if ($preException instanceof \Tymon\JWTAuth\Exceptions\TokenBlacklistedException) {
+                 return response()->json(['error' => 'TOKEN_BLACKLISTED']);
+           }
+           if ($exception->getMessage() === 'Token not provided') {
+               return response()->json(['code' => 401, 'msg' => '未授权']);
+           }
+        }
+        /*return parent::render($request, $exception);*/
     }
 }
