87f3a015023c28796607c6024e9c0f464b1d2313
Suzhenwen <2663930382@qq.com>
2020-05-25 15:29:46 +0800

    app 支付

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -18,9 +18,27 @@ class OrderController extends Controller {
          * return  array
          */
     public function createOrder() {
-        $data = self::$accept_data;
+        $data['order_number'] = date('YmdHis', time()) . rand(1111, 9999);
+        $data['admin_id'] = 0;  //操作员id
+        $data['order_type'] = 2;        //1线下支付 2 线上支付
         $data['student_id'] = 1;
-        $orderid = Order::orderPayList($data);
-        return response()->json($orderid);
+        $data['price'] = 1;
+        $data['lession_price'] = 1;
+        $data['pay_status'] = 4;
+        $data['pay_type'] = 0;
+        $data['status'] = 0;
+        $data['oa_status'] = 0;              //OA状态
+        $data['class_id'] = 2;
+        $add = Order::insert($data);
+        print_r($add);
+
+
+
+
+
+//        $data = self::$accept_data;
+//        $data['student_id'] = 1;
+//        $orderid = Order::orderPayList($data);
+//        return response()->json($orderid);
     }
 }
