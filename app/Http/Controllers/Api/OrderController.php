b3dbde704ec5f6165319469480c8794616e306a7
Suzhenwen <2663930382@qq.com>
2020-05-29 17:26:52 +0800

    订单modle

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -141,10 +141,12 @@ class OrderController extends Controller
                 return ['code' => 201, 'msg' => '请选择订单'];
             }
             //获取订单信息
-            $order = Order::where(['id' => $data['order_id'], 'student_id' => $user_id])->first()->toArray();
+            $order = Order::where(['id' => $data['order_id'], 'student_id' => $user_id])->first();
             if(!$order){
                 return ['code' => 201, 'msg' => '订单数据有误'];
             }
+            $order->toArray();
+            print_r($order);die;
             if ($order['status'] > 0) {
                 return ['code' => 202, 'msg' => '此订单已支付'];
             }
