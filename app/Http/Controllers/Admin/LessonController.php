613c9f7667dcdbc4b36002f2ce1aadb292c3bc45
sxl <1275920140@qq.com>
2020-05-29 16:49:25 +0800

    公开课选填参数

--- app/Http/Controllers/Admin/LessonController.php
+++ app/Http/Controllers/Admin/LessonController.php
@@ -135,8 +135,6 @@ class LessonController extends Controller {
             'nickname' => 'required_if:is_public,1',
             'start_at' => 'required_if:is_public,1',
             'end_at' => 'required_if:is_public,1',
-            'barrage' => 'required_if:is_public,1',
-            'modetype' => 'required_if:is_public,1',
             'teacher_id' => 'required_if:is_public,1',
         ]);
 
