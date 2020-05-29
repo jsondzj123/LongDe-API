3b256a667ae7b2fd7fd87c055c8aa9d0a91b9709
Suzhenwen <2663930382@qq.com>
2020-05-29 17:14:14 +0800

    订单modle

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -163,6 +163,8 @@ class OrderController extends Controller
                 }
             }
             if ($data['pay_type'] == 5) {
+                echo $lesson['favorable_price'].'------------';
+                echo $user_balance;die;
                 if ($lesson['favorable_price'] > $user_balance) {
                     return ['code' => 202, 'msg' => '余额不足，请充值！！！！！'];
                 } else {
