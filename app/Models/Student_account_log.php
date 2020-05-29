237528f1be45974bf59d786db4e560691906c40a
Suzhenwen <2663930382@qq.com>
2020-05-29 11:27:40 +0800

    api 我的订单

--- app/Models/Student_account_log.php
+++ app/Models/Student_account_log.php
@@ -6,9 +6,9 @@ use App\Models\AdminLog;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\DB;
 
-class Student_price extends Model {
+class Student_account_log extends Model {
     //指定别的表名
-    public $table      = 'ld_student_price';
+    public $table      = 'ld_student_account_log';
     //时间戳设置
     public $timestamps = false;
 
