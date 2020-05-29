5842cdd74d4888373b2265127a08f448123ffd49
sxl <1275920140@qq.com>
2020-05-29 10:56:14 +0800

    学员账户表优化

--- database/migrations/2020_05_28_000001_create_ld_student_accounts_table.php
+++ database/migrations/2020_05_28_000001_create_ld_student_accounts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLdStudentPriceTable extends Migration
+class CreateLdStudentAccountsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -36,6 +36,6 @@ class CreateLdStudentPriceTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('ld_student_price');
+        Schema::dropIfExists('ld_student_accounts');
     }
 }
