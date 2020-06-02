c601088253bdf0ce1709e348e10a33bf72775652
sxl <1275920140@qq.com>
2020-06-02 15:48:04 +0800

    录播资源学科关联移除

--- database/migrations/2020_05_20_000006_create_ld_videos_table.php
+++ database/migrations/2020_05_20_000006_create_ld_videos_table.php
@@ -16,7 +16,6 @@ class CreateLdVideosTable extends Migration
         Schema::create('ld_videos', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('admin_id')->default(0)->comment('操作员ID');
-            $table->integer('subject_id')->unsigned()->comment('科目ID');
             $table->string('name')->comment('视频名称');
             $table->integer('category')->nullable()->default(0)->comment('类型:1视频2音频3课件4文档');
             $table->string('url')->comment('资源地址');
@@ -25,10 +24,6 @@ class CreateLdVideosTable extends Migration
             $table->integer('is_del')->nullable()->default(0)->comment('是否删除：0否1是');
             $table->integer('is_forbid')->nullable()->default(0)->comment('是否禁用：0否1是');
             $table->timestamps();
-
-            $table->foreign('subject_id')->references('id')->on('ld_subjects')
-                ->onUpdate('cascade')->onDelete('cascade');
-            $table->index('subject_id');
         });
     }
 
