7dab87d8bec40d5566a65c6ac9e8b33d2115ee0e
sxl <1275920140@qq.com>
2020-06-05 17:16:43 +0800

    进入课程移到API

--- app/Http/Controllers/Admin/LiveChildController.php
+++ app/Http/Controllers/Admin/LiveChildController.php
@@ -177,37 +177,4 @@ class LiveChildController extends Controller {
         }
         return $this->response("删除成功");
     }
-
-    //进入直播课程
-    public function courseAccess()
-    {
-         $validator = Validator::make($request->all(), [
-            'course_id' => 'required',
-            'student_id' => 'required',
-            'nickname' => 'required',
-            'role' => 'required',
-            'type' => 'required',
-         ]);
-         if ($validator->fails()) {
-             return $this->response($validator->errors()->first(), 202);
-         }
-        $MTCloud = new MTCloud();
-        // 参数：课程ID
-        // 进入直播课程
-         if($request->input('type') == 1){
-             $res = $MTCloud->courseAccess($request->input('course_id'), $request->input('student_id'), $request->input('nickname'),
-                    $request->input('role'));
-         }else{
-             $res = $MTCloud->courseAccessPlayback(
-                 $request->input('course_id'), 
-                 $request->input('student_id'), 
-                 $request->input('nickname'),
-                 $request->input('role'));
-        }
-        if(!array_key_exists('code', $res) && !$res['code'] == 0){
-            Log::error('进入直播间失败:'.json_encode($res));
-            return $this->response('进入直播间失败', 500);
-        }
-        return $this->response($res['data']);
-    }
 }
