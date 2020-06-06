0233b1e2264474effcf5cc816962b9c9cb8eb03c
sunxiaoli <1275920140@qq.com>
2020-06-06 16:27:13 +0800

    欢拓配置

--- app/Tools/MTCloud.php
+++ app/Tools/MTCloud.php
@@ -18,12 +18,12 @@ class MTCloud{
     /*
     *  合作方ID：欢拓平台的唯一ID
     */
-    private $openID = '';
+    private $openID = env('MT_OPEN_ID');
 
     /*
     *  合作方秘钥：欢拓平台唯一ID对应的加密秘钥
     */
-    private $openToken = '';
+    private $openToken = env('MT_OPEN_TOKEN');
 
     /*
     *   欢拓API接口地址
