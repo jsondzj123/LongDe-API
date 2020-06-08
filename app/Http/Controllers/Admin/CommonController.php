fd5a67e7560ecd2ed0ec7bb85a2ad839f3a24f37
jsondzj123 <duzhijian11@163.com>
2020-06-08 14:34:26 +0800

    1.增加OSS图片返回json格式 2.增加删除OSS操作的方法

--- app/Http/Controllers/Admin/CommonController.php
+++ app/Http/Controllers/Admin/CommonController.php
@@ -119,20 +119,20 @@ class CommonController extends BaseController {
 
             //判断是否有文件上传
             if(!isset($_FILES['file']) || empty($_FILES['file']['tmp_name'])){
-                return ['code' => 201 , 'msg' => '请上传图片文件'];
+                return response()->json(['code' => 201 , 'msg' => '请上传图片文件']);
             }
             
             //获取上传文件的文件后缀
             $is_correct_ext = \App\Http\Controllers\Controller::detectUploadFileMIME($file);
             $image_extension= substr($_FILES['file']['name'], strrpos($_FILES['file']['name'], '.')+1);   //获取图片后缀名
             if($is_correct_ext <= 0 || !in_array($image_extension , ['jpg' , 'jpeg' , 'gif' , 'png'])){
-                return ['code' => 202 , 'msg' => '上传图片格式非法'];
+                return response()->json(['code' => 202 , 'msg' => '上传图片格式非法']);
             }
             
             //判断图片上传大小是否大于3M
             $image_size = filesize($_FILES['file']['tmp_name']);
             if($image_size > 3145728){
-                return ['code' => 202 , 'msg' => '上传图片不能大于3M'];
+                return response()->json(['code' => 202 , 'msg' => '上传图片不能大于3M']);
             }
 
             //存放文件路径
@@ -151,15 +151,15 @@ class CommonController extends BaseController {
                 //上传文件方法
                 $rs =  move_uploaded_file($_FILES['file']['tmp_name'], $path);
                 if($rs && !empty($rs)){
-                    return ['code' => 200 , 'msg' => '上传图片成功' , 'data' => "/upload/editor/" . date('Y-m-d') . '/'.$filename];
+                    return response()->json(['code' => 200 , 'msg' => '上传图片成功' , 'data' => "/upload/editor/" . date('Y-m-d') . '/'.$filename]);
                 } else {
-                    return ['code' => 203 , 'msg' => '上传图片失败'];
+                    return response()->json(['code' => 203 , 'msg' => '上传图片失败']);
                 }
             } else {
-                return ['code' => 202 , 'msg' => '上传方式非法'];
+                return response()->json(['code' => 202 , 'msg' => '上传方式非法']);
             }
         } catch (Exception $ex) {
-            return ['code' => 500 , 'msg' => $ex->getMessage()];
+            return response()->json(['code' => 500 , 'msg' => $ex->getMessage()]);
         }
     }
     
@@ -178,20 +178,20 @@ class CommonController extends BaseController {
 
             //判断是否有文件上传
             if(!isset($_FILES['file']) || empty($_FILES['file']['tmp_name'])){
-                return ['code' => 201 , 'msg' => '请上传图片文件'];
+                return response()->json(['code' => 201 , 'msg' => '请上传图片文件']);
             }
             
             //获取上传文件的文件后缀
             $is_correct_ext = \App\Http\Controllers\Controller::detectUploadFileMIME($file);
             $image_extension= substr($_FILES['file']['name'], strrpos($_FILES['file']['name'], '.')+1);   //获取图片后缀名
             if($is_correct_ext <= 0 || !in_array($image_extension , ['jpg' , 'jpeg' , 'gif' , 'png'])){
-                return ['code' => 202 , 'msg' => '上传图片格式非法'];
+                return response()->json(['code' => 202 , 'msg' => '上传图片格式非法']);
             }
             
             //判断图片上传大小是否大于3M
             $image_size = filesize($_FILES['file']['tmp_name']);
             if($image_size > 3145728){
-                return ['code' => 202 , 'msg' => '上传图片不能大于3M'];
+                return response()->json(['code' => 202 , 'msg' => '上传图片不能大于3M']);
             }
             
             //重置文件名
@@ -212,12 +212,51 @@ class CommonController extends BaseController {
             //上传图片到OSS
             $getOssInfo = $ossClient->uploadFile($image_config['bucket'] , $path , $_FILES['file']['tmp_name'] , [OssClient::OSS_CONTENT_TYPE => 'image/jpg']);
             if($getOssInfo && !empty($getOssInfo)){
-                return ['code' => 200 , 'msg' => '上传成功' , 'data' => $getOssInfo['info']['url']];
+                return response()->json(['code' => 200 , 'msg' => '上传成功' , 'data' => $getOssInfo['info']['url']]);
             } else {
-                return ['code' => 203 , 'msg' => '上传失败'];
+                return response()->json(['code' => 203 , 'msg' => '上传失败']);
             }
         } catch (Exception $ex) {
-            return ['code' => 500 , 'msg' => $ex->getMessage()];
+            return response()->json(['code' => 500 , 'msg' => $ex->getMessage()]);
+        }
+    }
+    
+    
+    /*
+     * @param  description   删除OSS上面的图片方法
+     * @param $filename      文件的信息(例如:/upload/2020-06-08/159158885017335eddb7f232739.jpg)
+     * @param author    dzj
+     * @param ctime     2020-06-08
+     * return string
+     */
+    public function doDeleteOssImage($filename = ''){
+        try {
+            //oss图片公共参数配置部分
+            $image_config = [
+                'accessKeyId'     =>   env('OSS_IMAGE_ACCESSKEYID') ,
+                'accessKeySecret' =>   env('OSS_IMAGE_ACCESSKEYSECRET') ,
+                'bucket'          =>   env('OSS_IMAGE_BUCKET') ,
+                'oss_url'         =>   env('OSS_IMAGE_URL')
+            ];
+            
+            //上传图片到阿里云OSS服务器上面
+            $ossClient = new \OSS\OssClient($image_config['accessKeyId'] , $image_config['accessKeySecret'] , $image_config['oss_url']);
+            
+            //先判断图片是否存在于OSS服务器上面
+            $exists_image = $ossClient->doesObjectExist($image_config['bucket'] , $filename);
+            if($exists_image && !empty($exists_image)) {
+                //执行删除OSS图片文件操作
+                $rs = $ossClient->deleteObject($image_config['bucket'] , $filename);
+                if($rs && !empty($rs)){
+                    return response()->json(['code' => 200 , 'msg' => '删除成功' , 'data' => $rs]);
+                } else {
+                    return response()->json(['code' => 203 , 'msg' => '删除失败']);
+                }
+            } else {
+                return response()->json(['code' => 203 , 'msg' => '此图片不存在']);
+            }
+        } catch (Exception $ex) {
+            return response()->json(['code' => 500 , 'msg' => $ex->getMessage()]);
         }
     }
 }
