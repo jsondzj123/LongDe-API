a4175d1c1bf2fb72ee1548418ed46bfe25c87009
jsondzj123 <duzhijian11@163.com>
2020-06-08 17:29:04 +0800

    去掉课程状态

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -194,12 +194,12 @@ class IndexController extends Controller {
                 foreach($teacher_list as $k=>$v){
                     //根据大分类的id获取大分类的名称
                     if($v['parent_id'] && $v['parent_id'] > 0){
-                        $lession_parent_name = Subject::where("id" , $v['parent_id'])->where("status" , 1)->where("is_del" , 0)->where("is_forbid" , 0)->value("name");
+                        $lession_parent_name = Subject::where("id" , $v['parent_id'])->where("is_del" , 0)->where("is_forbid" , 0)->value("name");
                     }
 
                     //根据小分类的id获取小分类的名称
                     if($v['child_id'] && $v['child_id'] > 0){
-                        $lession_child_name  = Subject::where("id" , $v['child_id'])->where("status" , 1)->where("is_del" , 0)->where("is_forbid" , 0)->value("name");
+                        $lession_child_name  = Subject::where("id" , $v['child_id'])->where("is_del" , 0)->where("is_forbid" , 0)->value("name");
                     }
 
                     //数组赋值
@@ -385,12 +385,12 @@ class IndexController extends Controller {
                 foreach($famous_teacher_list as $k=>$v){
                     //根据大分类的id获取大分类的名称
                     if($v['parent_id'] && $v['parent_id'] > 0){
-                        $lession_parent_name = Subject::where("id" , $v['parent_id'])->where("status" , 1)->where("is_del" , 0)->where("is_forbid" , 0)->value("name");
+                        $lession_parent_name = Subject::where("id" , $v['parent_id'])->where("is_del" , 0)->where("is_forbid" , 0)->value("name");
                     }
 
                     //根据小分类的id获取小分类的名称
                     if($v['child_id'] && $v['child_id'] > 0){
-                        $lession_child_name  = Subject::where("id" , $v['child_id'])->where("status" , 1)->where("is_del" , 0)->where("is_forbid" , 0)->value("name");
+                        $lession_child_name  = Subject::where("id" , $v['child_id'])->where("is_del" , 0)->where("is_forbid" , 0)->value("name");
                     }
                     
                     //获取课程的id列表
