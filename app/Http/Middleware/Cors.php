cf7b2352f99e6e2969ce4d14a761f72c985e0148
sxl <1275920140@qq.com>
2020-06-09 18:54:19 +0800

    update cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,6 +16,7 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
+        ->header('Access-Control-Allow-Credentials', 'true')
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Expose-Headers', 'Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers, X-Requested-By, Access-Control-Allow-Methods')
             ->header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
