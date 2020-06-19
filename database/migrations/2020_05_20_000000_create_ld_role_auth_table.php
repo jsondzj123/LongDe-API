7a4d3fa67c87050930cb5aab8cdfa23485ba51f7
Suzhenwen <2663930382@qq.com>
2020-06-19 15:36:16 +0800

    表修改

--- database/migrations/2020_05_20_000000_create_ld_role_auth_table.php
+++ database/migrations/2020_05_20_000000_create_ld_role_auth_table.php
@@ -24,7 +24,9 @@ class CreateLdRoleAuthTable extends Migration
             $table->smallInteger('is_super')->default(1)->comment('超级管理员0否1是');
             $table->smallInteger('is_del')->default(1)->comment('删除0是1否');
             $table->integer('school_id')->comment('学校ID');
-            $table->index('auth_id');
+
+
+            $table->index('auth_id' , 'index_auth_id');
         });
         DB::statement("alter table `ld_role_auth` comment '角色表'");
     }
